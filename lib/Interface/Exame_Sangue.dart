98662778137d68bad5905b66315db68a344b4ff0
Alice Novello <137834806+alicenovello@users.noreply.github.com>
2024-10-05 09:50:11 -0300

    .

--- lib/Interface/Exame_Sangue.dart
+++ lib/Interface/Exame_Sangue.dart
@@ -15,13 +15,19 @@ class ExameSangue {
 }
 
 List<ExameSangue> exames = [
-  ExameSangue(nome: 'Hemoglobina', unidade: 'g/dL', valorMin: 13.5, valorMax: 17.5),
-  ExameSangue(nome: 'Glicose', unidade: 'mg/dL', valorMin: 70, valorMax: 100),
+  ExameSangue(nome: 'Hemoglobina', unidade: 'milhões/µL', valorMin: 13.5, valorMax: 17.5),
+  ExameSangue(nome: 'Eritrócitos', unidade: 'g/dL', valorMin: 4.3, valorMax: 5.7),
+  ExameSangue(nome: 'Hematócrito', unidade: '%', valorMin: 39, valorMax: 50),
+  ExameSangue(nome: 'H.C.M.', unidade: 'pg', valorMin: 26, valorMax: 34),
+  ExameSangue(nome: 'V.C.M', unidade: 'fL', valorMin: 80, valorMax: 99),
+  ExameSangue(nome: 'C.H.C.M', unidade: 'g/dL', valorMin: 31, valorMax: 36),
+  ExameSangue(nome: 'R.D.W', unidade: '', valorMin: 12, valorMax: 14.9),
   ExameSangue(nome: 'Plaquetas', unidade: 'mil/mm³', valorMin: 150, valorMax: 450),
-  ExameSangue(nome: 'Colesterol Total', unidade: 'mil/mm³', valorMin: 0, valorMax: 190),
-  ExameSangue(nome: 'Colesterol HDL', unidade: 'mil/mm³', valorMin: 40, valorMax: 190),
-  ExameSangue(nome: 'Colesterol não HDL', unidade: 'mil/mm³', valorMin: 0, valorMax: 160),
-  ExameSangue(nome: 'Colesterol LDL', unidade: 'mil/mm³', valorMin: 0, valorMax: 130),
+  ExameSangue(nome: 'Glicose', unidade: 'mg/dL', valorMin: 70, valorMax: 100),
+  ExameSangue(nome: 'Colesterol Total', unidade: 'mg/dL', valorMin: 0, valorMax: 190),
+  ExameSangue(nome: 'Colesterol HDL', unidade: 'mg/dL', valorMin: 40, valorMax: 190),
+  ExameSangue(nome: 'Colesterol não HDL', unidade: 'mg/dL', valorMin: 0, valorMax: 160),
+  ExameSangue(nome: 'Colesterol LDL', unidade: 'mg/dL', valorMin: 0, valorMax: 130),
   ExameSangue(nome: 'Triglicérides', unidade: 'mg/dL', valorMin: 0, valorMax: 150),
 ];
 
