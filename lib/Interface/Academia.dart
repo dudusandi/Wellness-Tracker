e9dac2dda68ef3a964acf9d5475a4955b5e0a798
Eduardo Sandi <dudaesouza@gmail.com>
2024-10-19 00:04:41 -0300

    academia

--- lib/Interface/Academia.dart
+++ lib/Interface/Academia.dart
@@ -8,8 +8,16 @@ class Academia extends StatefulWidget {
 class _AcademiaState extends State<Academia> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Academia', style: TextStyle(color: Colors.black, fontSize: 24)),
-    );
+    return Container(
+        color: Color(0xFF472B2B),
+        padding: EdgeInsets.all(60),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(
+            'Academia',
+            style: TextStyle(
+                color: Colors.white, fontSize: 32, fontFamily: 'JuliusSansOne'),
+          ),
+          SizedBox(height: 80),
+        ]));
   }
 }
