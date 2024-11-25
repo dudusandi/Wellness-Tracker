1843a1b7246395c1d2a96fd9c78486525cf64221
Duda <dudaesouza@gmail.com>
2024-11-25 04:25:02 -0300

    fix banco mac

--- lib/Model/CriarBanco.dart
+++ lib/Model/CriarBanco.dart
@@ -4,6 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqlite3/sqlite3.dart';
 
 class CriarBanco {
+  
   Future<void> criarBancoDeDados() async {
     late String path;
 
